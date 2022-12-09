8d0e3df15603914f535ecd9d09a5a84d9da11803
Anwarnurudin <108691739+Anwarnurudin@users.noreply.github.com>
2022-12-09 15:57:04 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -1,5 +1,23 @@
 import pickle
 import streamlit as st
+import base64
+
+#import background
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('1905040037.jpg')
 
 # membaca model
 diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
